95994ed3c8917fcc5ae2a8f1f9419469531d687a
Neil-Ni <tzuyangni@gmail.com>
2013-10-18 19:44:24 +0000

    physics calculator api started

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, g, session, make_response, render_template, \
     flash, redirect, url_for
+from flask.ext import restful
+
 app = Flask(__name__)
+api = restful.Api(app)
 @app.route('/')
 def home():
     return make_response(open('index.html').read())
@@ -9,8 +12,14 @@ def blog():
     return make_response(open('blog/index.php').read())
 @app.route('/calculator')
 def calculator():
-#    return 'calculator page'
-    return make_response(open('physicscalculator/index.html').read())
+    return make_response(open('physicscalculator/app/index.html').read())
+
+
+class CalculatorAPI(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(CalculatorAPI, '/calculator/api')
 
 if __name__ == '__main__':
     app.run(port=8000)
